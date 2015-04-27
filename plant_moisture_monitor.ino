931225f7d2649313670e21cb897fda32ecd6e0a1
Tanel Lebedev <tanel.lebedev@gmail.com>
2015-04-27 13:27:44 +0300

    read actual hygrometer input

--- plant_moisture_monitor.ino
+++ plant_moisture_monitor.ino
@@ -1,37 +1,70 @@
 
-typedef struct pottype_ {
+typedef struct pot_type_ {
     int yellow;
     int red;
 } pot_t;
 
-// configuration
-int MINIMUM_MOISTURE_LEVEL = 20;
-int SLEEP_SECONDS = 30;
-pot_t POTS[] = {{13, 12}, {11, 10}};
+typedef struct hygrometer_type_ {
+    int digital_output;
+    int analog_input;
+} hygrometer_t;
 
-int potcount = sizeof(POTS) / sizeof(POTS[0]);
+// configuration
+int MINIMUM_MOISTURE_LEVEL = 400;
+pot_t POTS[] = {{13, 12}, {11, 10}};
+hygrometer_t HYGROMETERS[] = {{7, A1}, {6, A0}};
+int CHECK_INTERVAL_MINUTES = 30;
+
+int pot_count = sizeof(POTS) / sizeof(POTS[0]);
+int hygrometer_count = sizeof(HYGROMETERS) / sizeof(HYGROMETERS[0]);
 
 void setup() {
-  for (int i = 0; i < potcount; i++) {
+  Serial.begin(9600);
+  Serial.println("starting");
+
+  for (int i = 0; i < hygrometer_count; i++) {
+    pinMode(HYGROMETERS[i].digital_output, OUTPUT);
+    digitalWrite(POTS[i].yellow, LOW);
+  }
+
+  for (int i = 0; i < pot_count; i++) {
     pinMode(POTS[i].yellow, OUTPUT);
-    digitalWrite(POTS[i].yellow, HIGH);
+    digitalWrite(POTS[i].yellow, LOW);
 
     pinMode(POTS[i].red, OUTPUT);
-    digitalWrite(POTS[i].red, HIGH);
+    digitalWrite(POTS[i].red, LOW);
   }
 }
 
 void loop() {
-  for (int i = 0; i < potcount; i++) {
+  for (int i = 0; i < pot_count; i++) {
     digitalWrite(POTS[i].yellow, HIGH);
-    delay(1000); // remove after adding real measurement code
-    int measurement = 20;
-    if (measurement < MINIMUM_MOISTURE_LEVEL) {
+
+    digitalWrite(HYGROMETERS[i].digital_output, HIGH);
+
+    delay(1000);
+
+    int measurement = analogRead(HYGROMETERS[i].analog_input);
+
+    Serial.print("measurement of hygrometer ");
+    Serial.print(i);
+    Serial.print(" is ");
+    Serial.println(measurement);
+
+    if (measurement > MINIMUM_MOISTURE_LEVEL) {
       digitalWrite(POTS[i].red, HIGH);
     } else {
       digitalWrite(POTS[i].red, LOW);
     }
+
+    digitalWrite(HYGROMETERS[i].digital_output, LOW);
+
     digitalWrite(POTS[i].yellow, LOW);
   }
-  delay(SLEEP_SECONDS * 1000);
+
+  Serial.println("sleeping");
+  for (int i = 0; i < CHECK_INTERVAL_MINUTES; i++) {
+    delay(60 * 1000);
+  }
+  Serial.println("sleep done");
 }
